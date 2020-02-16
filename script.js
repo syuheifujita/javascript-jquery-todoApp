cafc5d95b40465d93a51a85f6323dae4e7f0f8c7
syuheifujita <mcslr772ed@gmail.com>
2020-02-16 14:58:26 +0900

    modify

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $(function() {
+    var arrayTodoList = new Array();
+    
     $('#form').submit(function() {
         var textValue = $('#text-form').val();
         $('#output-text').text(textValue);
 
+        //配列をリスト表示
+        arrayTodoList.push(textValue);
+
         return false;
     });
+
+    $.each(arrayTodoList, function (index, element) {
+        $('#output-text').append(index + ':' + element + '<br>');
+      })
 });
