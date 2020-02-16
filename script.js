777475f8c6558aa45359c07590f3e6dfd383aad6
syuheifujita <mcslr772ed@gmail.com>
2020-02-16 13:42:00 +0900

    modify

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-$(Document).ready(function() {
+$(function() {
+    $('#form').submit(function() {
+        var textValue = $('#text-form').val();
+        $('#output-text').text(textValue);
 
-    
+        return false;
+    });
 });
