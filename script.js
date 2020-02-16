18b71528f7596f43e1a57b52f5b8387eb873a08d
syuheifujita <mcslr772ed@gmail.com>
2020-02-16 23:25:11 +0900

    modify

--- script.js
+++ script.js
@@ -12,14 +12,31 @@ $(function() {
         //配列に要素を追加
         arrayTodoList.push(textValue);
 
-        // 配列を表示させる
+        // 配列を表示
         $.each(arrayTodoList, function (index, element) {
-            $('#output-text').append(index + ':' + element + '<br>');
+            $('#output-text').append((index + 1) + ': ' + element + '<br>');
         })
 
         // 入力フォームのテキストを初期化
-        $('#text-form').empty();
+        // $('#text-form').empty();
 
         return false;
+    });
+
+    $('output-text').on('click',function() {
+        var id = $(this).attr("id");
+        alert(id);
+
+        // クリックされたときに，モーダルのタグが表示されるようにする
+        $("body").append('<div id="modal-bg"></div>');
+        $("#modal-bg").fadeIn("slow");
+
+        //画面のどこかをクリックしたらモーダルを閉じる
+        $("#modal-bg").click(function(){
+            $("#modal-bg").fadeOut("slow",function(){
+            //挿入した<div id="modal-bg"></div>を削除
+                $('#modal-bg').remove() ;
+            });
+        });
     });
 });
