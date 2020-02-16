b805cca36362757d91f2658431a6bcff1260e365
syuheifujita <mcslr772ed@gmail.com>
2020-02-16 22:41:55 +0900

    フォームに入力した時，配列に値を格納することができた

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 $(function() {
-    var arrayTodoList = new Array();
+    // 配列を初期化
+    var arrayTodoList = [];
     
     $('#form').submit(function() {
+        // フォームに入力されたテキストを，変数に格納
         var textValue = $('#text-form').val();
-        $('#output-text').text(textValue);
+        // $('#output-text').text(textValue);
 
-        //配列をリスト表示
+        //配列に要素を追加
         arrayTodoList.push(textValue);
+
+        // 配列を表示させる
+        $('output-text').text();
+
+        for(var i = 0; i < arrayTodoList.length; i++) {
+            console.log(arrayTodoList[i]);
+        }
+
+        $.each(arrayTodoList, function (index, element) {
+            $('#output-text').append(index + ':' + element + '<br>');
+        })
 
         return false;
     });
-
-    $.each(arrayTodoList, function (index, element) {
-        $('#output-text').append(index + ':' + element + '<br>');
-      })
 });
