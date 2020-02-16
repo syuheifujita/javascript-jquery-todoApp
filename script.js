0991a953028f369a0164fc370c7a5c61f9b99893
syuheifujita <mcslr772ed@gmail.com>
2020-02-16 22:55:16 +0900

    modify

--- script.js
+++ script.js
@@ -3,23 +3,22 @@ $(function() {
     var arrayTodoList = [];
     
     $('#form').submit(function() {
+        // #output-textを初期化
+        $('#output-text').empty();
+
         // フォームに入力されたテキストを，変数に格納
         var textValue = $('#text-form').val();
-        // $('#output-text').text(textValue);
 
         //配列に要素を追加
         arrayTodoList.push(textValue);
 
         // 配列を表示させる
-        $('output-text').text();
-
-        for(var i = 0; i < arrayTodoList.length; i++) {
-            console.log(arrayTodoList[i]);
-        }
-
         $.each(arrayTodoList, function (index, element) {
             $('#output-text').append(index + ':' + element + '<br>');
         })
+
+        // 入力フォームのテキストを初期化
+        $('#text-form').empty();
 
         return false;
     });
